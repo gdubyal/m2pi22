5904aa863a790c8b694217b3ec0158cb81d5184f
georgelee <georgelee@uvic.ca>
2022-07-22 11:49:18 -0700

    work in progress

--- water_ts_analysis.py
+++ water_ts_analysis.py
@@ -42,15 +42,18 @@ class WaterLevelAnalyser:
                                       dtype=n.int64)
 
     def plot_timeseries(self, overlay_event_starts=True, overlay_event_ends=True,
-                        overlay_peaks=True, overlay_flood_ends=True):
+                        overlay_peaks=True, overlay_flood_ends=True, plot_event=-1):
         '''
         Plot the timeseries being considered.
         '''
 
         t.plot(self.times_as_np, self.water_levels_mm)
+	evnts = zip(self.event_start_times, self.event_end_times, self.event_peaks_mm,
+                  self.flood_end_times) if plot_event == -1 else\
+                [[self.event_start_times[plot_event], self.event_end_times[plot_event],
+                  self.event_peaks_mm[plot_event], self.flood_end_times[plot_event]]
 
-        for i, j, peak, end in zip(self.event_start_times, self.event_end_times,
-                                   self.event_peaks, self.flood_end_times):
+        for i, j, peak, end in evnts:
 
             if overlay_event_starts:
                 t.axvline(x=i, alpha=.5, color='red')
@@ -74,53 +77,55 @@ class WaterLevelAnalyser:
         '''
 
         # Find all times when there was an level change above the threshold
-        self._increase_mask = self.water_level_changes > self.level_water_level_increase_threshold
-        self.events_mask = self._increase_mask.copy()
-        time_since_last_inc = n.timedelta64(99999999, 'm')
-        event_end_times = []
+        self._increase_mask= self.water_level_changes >
+                             self.level_water_level_increase_threshold
+        self.events_mask= self._increase_mask.copy()
+        time_since_last_inc= n.timedelta64(99999999, 'm')
+        event_end_times= []
 
         # Find event end times - defined as 6 hours after last rise in the event
         for i in range(len(self.events_mask)):
             if self.events_mask[i]:
                 if time_since_last_inc < self.event_time_gap:
-                    self.events_mask[i] = False
-                    event_end_times[-1] = self.times_as_np[i] + \
-                        self.event_time_gap
+                    self.events_mask[i]= False
+                    event_end_times[-1]= self.times_as_np[i] + self.event_time_gap
                 else:
                     event_end_times.append(
                         self.times_as_np[i]+self.event_time_gap)
-                time_since_last_inc = n.timedelta64()
+                time_since_last_inc= n.timedelta64()
             time_since_last_inc += self.frame_rate
 
-        self.event_end_times = n.array(event_end_times)
-        self.event_start_times = self.times_as_np[1:][self.events_mask]
+        self.event_end_times= n.array(event_end_times)
+        self.event_start_times= self.times_as_np[1:][self.events_mask]
 
     def find_peak_levels(self):
-        peaks = []
-        peak_entries = []
-        flood_end_times = []
+        peaks= []
+        peak_times= []
+        flood_end_times= []
         for start, end in zip(self.event_start_times, self.event_end_times):
-            event_start_index = n.where(self.times_as_np == start)[0][0]
-            event_end_index = n.where(self.times_as_np == end)[0][0]
-            peak_i = self.water_levels_mm[event_start_index:event_end_index].argmax(
-            )
-            peak_entries.append(event_start_index+peak_i)
+            event_start_index= n.where(self.times_as_np == start)[0][0]
+            event_end_index= n.where(self.times_as_np == end)[0][0]
+            peak_i= self.water_levels_mm[event_start_index:event_end_index].argmax()
+            peak_times.append(self.times_as_np[event_start_index+peak_i])
             peaks.append(self.water_levels_mm[event_start_index:event_end_index]
                                              [peak_i])
             flood_end_times.append(self.times_as_np[event_start_index+peak_i+n.where(
                                    self.water_levels_mm[event_start_index+peak_i:] <
                                    self.flood_threshold_mm)[0][0]])
 
-        self.event_peaks = n.array(peaks)
-        self.event_peaks_indices = n.array(peak_entries)
-        self.flood_end_times = n.array(flood_end_times)
+        self.event_peaks_mm= n.array(peaks)
+        self.event_peak_times= n.array(peak_times)
+        self.flood_end_times= n.array(flood_end_times)
+        self.drawdown_rates= n.timedelta64(1, 'm') *\
+	    (self.event_peaks_mm-self.flood_threshold_mm) /\
+            (self.flood_end_times-self.event_peak_times)
 
 
 if __name__ == '__main__':
 
     import argparse
 
-    prs = argparse.ArgumentParser(
+    prs= argparse.ArgumentParser(
         description='Water level timeseries analyser')
 
     # By default read site A
@@ -131,9 +136,9 @@ if __name__ == '__main__':
                      help='Flood level threshold (mm)')
     prs.add_argument('--water_level_increase_threshold', '-i', default=4, type=int,
                      help='Water level increase threshold (mm/time step)')
-    args = prs.parse_args()
-    analyser = WaterLevelAnalyser(args.csv, flood_threshold=args.flood_threshold,
-                                  water_level_increase_threshold=args.water_level_increase_threshold)
+    args= prs.parse_args()
+    analyser= WaterLevelAnalyser(args.csv, flood_threshold=args.flood_threshold,
+                    water_level_increase_threshold=args.water_level_increase_threshold)
 
     analyser.find_event_start_end_times()
     analyser.find_peak_levels()
