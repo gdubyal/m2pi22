7a637b9fdb7ea77e3f67390496c8e7510613f58a
georgelee <georgelee@uvic.ca>
2022-07-21 19:06:55 -0700

    Mark flood ends for given threshold after peak for events

--- water_ts_analysis.py
+++ water_ts_analysis.py
@@ -15,6 +15,7 @@ class WaterLevelAnalyser:
         self.rain_event_min_gap = self.unix1s*60*60*6  # 6 hours
         self.event_min_time_gap = 300
         self.data_rate = 300*self.unix1s
+        self.flood_threshold_mm=400
 
         # Read data,get useful quantities
         self._data = p.read_csv(csv_name, parse_dates=['Timestamp'])
@@ -23,8 +24,8 @@ class WaterLevelAnalyser:
 
     def compute_stats(self):
         self.water_levels_mm = (self._data[[' Water Level [m]']]*1000)\
-            .to_numpy(n.int64)\
-            .flatten()
+                               .to_numpy(n.int64)\
+                               .flatten()
         self.ts_times = self._data[['Timestamp']]
         self.times_as_int = self.ts_times.to_numpy(n.int64).flatten()
 
@@ -40,15 +41,16 @@ class WaterLevelAnalyser:
         self.time_intervals = n.array([self.data_rate for i in self.time_intervals],
                                       dtype=n.int64)
 
-    def plot_timeseries(self, overlay_event_starts=True,
-                        overlay_event_ends=True, overlay_peaks=True):
+    def plot_timeseries(self, overlay_event_starts=True, overlay_event_ends=True,
+                        overlay_peaks=True,overlay_flood_ends=True):
         '''
         Plot the timeseries being considered.
         '''
 
         t.plot(self.times_as_int, self.water_levels_mm)
 
-        for i, j, peak in zip(self.event_start_times, self.event_end_times, self.event_peaks):
+        for i, j, peak,end in zip(self.event_start_times, self.event_end_times,\
+                              self.event_peaks,self.flood_end_times):
 
             if overlay_event_starts:
                 t.axvline(x=i, alpha=.5, color='red')
@@ -57,9 +59,10 @@ class WaterLevelAnalyser:
                 t.axvline(x=j, alpha=.5, color='green')
 
             if overlay_peaks:
-                print(peak)
-                t.plot([i,j],[peak,peak],color='purple')
-                #t.axhline(y=peak,xmin=i,xmax=j,color='purple')
+                t.plot([i, j], [peak, peak], color='purple')
+            
+            if overlay_flood_ends and peak>self.flood_threshold_mm:
+                t.axvline(x=end,alpha=.5,color='blue')
 
         t.show()
 
@@ -74,30 +77,40 @@ class WaterLevelAnalyser:
         self._increase_mask = self.water_level_changes > self.level_increase_threshold
         self.events_mask = self._increase_mask.copy()
         time_since_last_inc = n.inf
-        _event_end_times = []
+        event_end_times = []
 
         # Find event end times - defined as 6 hours after last rise in the event
         for i in range(len(self.events_mask)):
             if self.events_mask[i]:
                 if time_since_last_inc < self.event_min_time_gap:
                     self.events_mask[i] = False
+                    event_end_times[-1] = self.times_as_int[i]+300*60*self.unix1s
                 else:
-                    _event_end_times.append(0)
-                _event_end_times[-1] = self.times_as_int[i]+300*60*self.unix1s
+                    event_end_times.append(self.times_as_int[i]+300*60*self.unix1s)
                 time_since_last_inc = 0
             time_since_last_inc += 5
 
-        self.event_end_times = n.array(_event_end_times)
+        self.event_end_times = n.array(event_end_times)
         self.event_start_times = self.times_as_int[1:][self.events_mask]
 
     def find_peak_levels(self):
-        _peaks = []
+        peaks = []
+        peak_entries=[]
+        flood_end_times=[]
         for start, end in zip(self.event_start_times, self.event_end_times):
-            event_start_index = n.where(self.times_as_int==start)[0][0]
-            event_end_index = n.where(self.times_as_int==end)[0][0]
-            _peaks.append(self.water_levels_mm[event_start_index:event_end_index].max())
+            event_start_index = n.where(self.times_as_int == start)[0][0]
+            event_end_index = n.where(self.times_as_int == end)[0][0]
+            peak_i=self.water_levels_mm[event_start_index:event_end_index].argmax()
+            peak_entries.append(event_start_index+peak_i)
+            peaks.append(self.water_levels_mm[event_start_index:event_end_index]\
+                                             [peak_i])
+            flood_end_times.append(self.times_as_int[event_start_index+peak_i+n.where(\
+                                   self.water_levels_mm[event_start_index+peak_i:]<\
+                                   self.flood_threshold_mm)[0][0]])
 
-        self.event_peaks = n.array(_peaks)
+        self.event_peaks = n.array(peaks)
+        self.event_peaks_indices=n.array(peak_entries)
+        self.flood_end_times=n.array(flood_end_times)
 
     def detect_implied_rainfall(self):
         '''
@@ -114,4 +127,5 @@ if __name__ == '__main__':
     analyser.find_peak_levels()
     analyser.plot_timeseries(overlay_event_starts=True,
                              overlay_event_ends=True,
-                             overlay_peaks=True)
+                             overlay_peaks=True,
+                 overlay_flood_ends=True)
